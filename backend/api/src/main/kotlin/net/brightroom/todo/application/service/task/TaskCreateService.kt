87bb262f60eb006e18b1ea0b8f3d50d86dea9471
kukv <koki-nonaka@outlook.jp>
2025-07-26 05:31:11 +0900

    apply spotless

--- backend/api/src/main/kotlin/net/brightroom/todo/application/service/task/TaskCreateService.kt
+++ backend/api/src/main/kotlin/net/brightroom/todo/application/service/task/TaskCreateService.kt
@@ -2,4 +2,6 @@ package net.brightroom.todo.application.service.task
 
 import net.brightroom.todo.application.repository.task.TaskCreateRepository
 
-class TaskCreateService(private val taskCreateRepository: TaskCreateRepository) : TaskCreateRepository by taskCreateRepository
+class TaskCreateService(
+    private val taskCreateRepository: TaskCreateRepository,
+) : TaskCreateRepository by taskCreateRepository
