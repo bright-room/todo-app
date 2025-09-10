0c60cf989834803c4bd9284417a972422abb6be8
kukv <koki-nonaka@outlook.jp>
2025-09-11 01:22:40 +0900

    WIP

--- backend/infrastructure/schemas/src/main/kotlin/net/brightroom/schemas/schema/todo/TaskIdTable.kt
+++ backend/infrastructure/schemas/src/main/kotlin/net/brightroom/schemas/schema/todo/TaskIdTable.kt
@@ -10,6 +10,5 @@ import kotlin.uuid.ExperimentalUuidApi
 
 @Migratable
 object TaskIdTable : KotlinUUIDTable("task_id") {
-    val account_id = reference("account_id", AccountIdTable)
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime).index()
 }
